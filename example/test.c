177bb2d4e569e663e813c9967bd8973cc88df740
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-12 07:37:32 +0330

    Update test.c

--- example/test.c
+++ example/test.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "ironveil.h"
 
+int main() {
+    const char *username = "amin";
+    const char *password = "hs3l5ndDF1@$hf"; // Relatively strong password
+
+  // To encrypt a text file
+    if (!iv_encrypt_file("document.txt", "document.ivl", username, password)) {
+        printf("Encryption failed\n");
+        return 1;
+    }
+    std::cout << "File encrypted successfully!" << std::endl;
+
+  // To decrypt a text file
+    if (!iv_decrypt_file("document.ivl", "document_decrypted.txt", username, password)) {
+        printf("Decryption failed (wrong username/password?)\n");
+        return 1;
+    }
+    printf("File decrypted successfully!\n");
+
+    return 0;
+}
