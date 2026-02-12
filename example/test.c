ab4aa278e98daf22520a85bd3279c93e490b92e1
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-12 06:28:13 +0330

    Implement file encryption and decryption
    Added functionality to encrypt and decrypt files using specified username and password.

--- example/test.c
+++ example/test.c
@@ -1,1 +1,26 @@
+#include <iostream>
+extern "C" {
+    #include "ironveil.h"
+}
+
+int main() {
+    const char *username = "amin";
+    const char *password = "hs3l5ndDF1@$hf"; // Relatively strong password
+
+  // To encrypt a text file
+    if (!iv_encrypt_file("document.txt", "document.ivl", username, password)) {
+        std::cerr << "❌ Encryption failed" << std::endl;
+        return 1;
+    }
+    std::cout << "✅ File encrypted successfully!" << std::endl;
+
+  // To decrypt a text file
+    if (!iv_decrypt_file("document.ivl", "document_decrypted.txt", username, password)) {
+        std::cerr << "❌ Decryption failed (wrong username/password?)" << std::endl;
+        return 1;
+    }
+    std::cout << "✅ File decrypted successfully!" << std::endl;
+
+    return 0;
+}
 
